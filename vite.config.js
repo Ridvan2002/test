b00dc0205657574d07f17bcc15ee84a23c28f7d4
Ridvan2002 <ridvansuleymanov28@gmail.com>
2024-10-06 00:17:45 -0400

    idk

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/test/' : '/',
+  base: '/test/',  // This should be the correct base path
   plugins: [react()],
 });
